7ff9a87096a4763cd62638b362065725c495aef5
Jordan Hilsman <jordanhilsman@gmail.com>
2024-10-05 13:42:50 -0400

    Added info for getting lines (future math)

--- get_week_games.py
+++ get_week_games.py
@@ -1,6 +1,9 @@
 import cfbd
 import os
 import subprocess
+import pandas as pd
+
+from tqdm import tqdm
 
 configuration = cfbd.Configuration()
 configuration.api_key["Authorization"] = os.getenv("CFBD_API_KEY")
@@ -10,8 +13,9 @@ api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
 week_of_choice = int(input("What week games do you want to grab? "))
 
 games = api_instance.get_games(year=2024, week=week_of_choice, division='fbs')
+betting_api = cfbd.BettingApi(cfbd.ApiClient(configuration))
 
-for game in games:
+for game in tqdm(games, total=len(games)):
     away_team = game.away_team
     home_team = game.home_team
     print(f"Getting {home_team} vs {away_team}")
@@ -20,3 +24,13 @@ for game in games:
                     "--away_team", away_team,
                     "--year", "2024", "--save"])
 
+print("Getting moneyline information for games...")
+for file in tqdm(os.listdir('./game_predictions/')):
+    df = pd.read_csv(f"./game_predictions/{file}")
+    home_team = file.split('___')[0]
+    lines = api_instance.get_lines(year=2024, week=6, team=home_team)
+    hml = lines[0].lines[0].home_moneyline
+    aml = lines[0].lines[0].away_moneyline
+    df['home_ml'] = hml
+    df['away_ml'] = aml
+    df.to_csv(f"./game_predictions/{file}", index=False)
